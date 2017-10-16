feec666ef3056a7cee2fe675c9f6520025fadc47
Mike Jones <mike@netsplit.org.uk>
2017-10-16 11:55:28 +0100

    Add MA releases module

---
+++ lib/Metal/Module/Releases.pm
@@ -0,0 +1,65 @@
+package Metal::Module::Releases;
+
+use JSON::XS    'decode_json';
+use LWP::Simple 'get';
+use Moose;
+
+extends 'Metal::Module';
+with    'Metal::Role::Logger';
+
+################################################################################
+
+has base_url => (is => 'ro', isa => 'Maybe[Str]', lazy_build => 1);
+has commands => (is => 'ro', isa => 'HashRef',    lazy_build => 1);
+
+################################################################################
+
+sub on_bot_public {
+    my $self  = shift;
+    my $event = shift;
+
+    my $output = $self->_attempt_command($event->{args});
+
+    $self->bot->message_channel($event->{args}->{channel}, $output);
+
+    return 1;
+}
+
+################################################################################
+
+sub _releases {
+    my $self = shift;
+    my $args = shift;
+
+    unless ($self->base_url) {
+        $self->logger->warn("releases:base_url config variable missing");
+
+        return;
+    }
+
+    my $json    = get($self->base_url);
+    my $content = decode_json($json);
+
+    return $content->{display} || '';
+}
+
+################################################################################
+
+sub _build_base_url {
+    my $self = shift;
+
+    return $self->bot->config->{releases}->{base_url};
+}
+
+sub _build_commands {
+    return {
+        releases => '_releases',
+    };
+}
+
+################################################################################
+
+no Moose;
+__PACKAGE__->meta->make_immutable();
+1;
+
